c2660a7553a07e3b6b9cfb46f30036f44e9b87aa
Felipe <guima.felipec@gmail.com>
2026-02-18 22:25:29 -0800

    Audio manager and clean ups

--- Scenes/Singletons/game_progress_manager.gd
+++ Scenes/Singletons/game_progress_manager.gd
@@ -1,6 +1,5 @@
 extends Node
 
-var current_level : int
 var max_level_reached: int = 0
 var last_level : int
 
@@ -22,19 +21,14 @@ func load_game():
 	max_level_reached = info["dingodiles"]
 	save_game.close()
 
-func set_max_level_reached(value: int):
+func set_max_level_reached(value: int) -> void:
 	if(max_level_reached > value):
 		return
 	max_level_reached = value
 	save()
-
-func set_current_level(level: int) -> void:
-	current_level = level
-	max_level_reached = max(max_level_reached, level)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	load_game()
 	var levels = ResourceLoader.list_directory("res://Scenes/Levels")
 	last_level = levels.size()
-	current_level = 1
