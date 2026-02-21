e20c9f765558db9336f86c5603de6fe385d2a9dd
Felipe <guima.felipec@gmail.com>
2026-02-21 14:33:54 -0800

    Sublte blur and code cleanup

--- Scenes/devil_animation.gd
+++ Scenes/devil_animation.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 signal lines_ended
 
+const max_blur := 3.0
+const devil_animation_duration := 0.2
+const blur_change_duration := 1.0
+
 var is_active := false
 var is_devil_found := false
 var lines: PackedStringArray
@@ -26,6 +30,23 @@ func animate_line() -> void:
 	await tween.finished
 	is_active = true
 
+func set_blur_core(blur_level: float) -> void:
+	blur_material.set_shader_parameter("blur_amount", blur_level)
+
+func set_blur(on: bool) -> void:
+	const min_blur := 0.0
+	var init : float
+	var end : float
+	var tween = self.create_tween()
+	if on:
+		init = min_blur
+		end = max_blur
+	else:
+		init = max_blur
+		end = min_blur
+	
+	tween.tween_method(set_blur_core, init, end, blur_change_duration)
+
 func move_animation(up: bool) -> void:
 	var devil_animation_tween = self.create_tween()
 	var destination: Vector2
@@ -37,10 +58,10 @@ func move_animation(up: bool) -> void:
 		destination = Vector2(get_viewport_rect().get_center().x, destination_y)
 		
 	
-	devil_animation_tween.tween_property(pivot, "global_position", destination, 0.2)
+	devil_animation_tween.tween_property(pivot, "global_position", destination, devil_animation_duration)
 	await devil_animation_tween.finished
 	if not up:
-		blur_material.set_shader_parameter("is_active", false)
+		set_blur(false)
 
 func show_next_line() -> void:
 	if current_line + 1 < lines.size():
@@ -62,7 +83,7 @@ func devil_found():
 	if is_devil_found:
 		return
 	
-	blur_material.set_shader_parameter("is_active", true)
+	set_blur(true)
 	is_devil_found = true
 	move_animation(true)
 	animate_line()
@@ -72,6 +93,7 @@ func _ready() -> void:
 	pivot = $DemonPivot
 	demon_sprite = $DemonPivot/DemonSprite
 	blur_material = $Blur.material
+	blur_material.set_shader_parameter("blur_amount", 0.0)
 	$Blur.global_position = Vector2(0, 0)
 	chat_box.visible_ratio = 0
 	var viewport_rect := get_viewport_rect()
