2202feec5016fe5085d633f406a483b14aa9d365
Felipe <guima.felipec@gmail.com>
2026-02-20 21:08:35 -0800

    Changing music volume while demon is talking

--- Scenes/global_audio_manager.gd
+++ Scenes/global_audio_manager.gd
@@ -3,12 +3,15 @@ extends Node
 var yaps : PackedStringArray
 
 func demon_talk() -> void:
+	$Music.volume_linear = 0.5
 	$Yap.stop()
 	var num_yaps = yaps.size()
 	randomize()
 	var i = randi() % num_yaps
 	$Yap.stream = ResourceLoader.load("res://Assets/Audio/Voices/GnomoA" + str(i+1) + ".wav")
 	$Yap.play()
+	await $Yap.finished
+	$Music.volume_linear = 1.0
 
 func _ready() -> void:
 	yaps = ResourceLoader.list_directory("res://Assets/Audio/Voices")
